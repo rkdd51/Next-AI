5511be3238d651ea4ae257a23c087054d2aad699
rahulDubey <rdubey@horizontal.com>
2023-05-18 11:20:33 +0530

    Basic Project setup with Routes and Dependency Added

---
+++ pages/post/new.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPost = () => {
+  return <div>Hello this is new post</div>;
+};
+
+export default NewPost;
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TokenTopup = () => {
+  return <div>This is Token topup page</div>;
+};
+
+export default TokenTopup;
