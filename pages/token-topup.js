5835ee324799575446ea3cd871a05893b6410f02
rahulDubey <rdubey@horizontal.com>
2023-05-18 13:24:03 +0530

    Added Authentication using auth0

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 const TokenTopup = () => {
   return <div>This is Token topup page</div>;
 };
 
 export default TokenTopup;
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {},
+  };
+});
